4820406dfc8db5fe509864bfa12cbb4bcade8fb2
DBM <diegobarmor42@gmail.com>
2025-02-23 14:24:30 +0100

    [WIP] improved termux-io

--- utilities/termux-io/pack_termux.sh
+++ utilities/termux-io/pack_termux.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+this_root="$(pwd)"
+folder_io=$(echo $0 | cut -d'/' -f 1)
 termux_root="/data/data/com.termux/files/home"
-this_root="$(pwd)/.."
 
-if [[ "$termux_root/Desktop/.." == "$this_root" ]]; then # runing inside termux
-    folder_out="$termux_root/storage/shared/TermuxIO"
+# -----------------------------------------------
+if [[ "$this_root" == "$termux_root" ]]; then # runing inside termux
+    folder_out="$this_root/storage/shared/TermuxIO"
 else
-    folder_out="$this_root/../../TermuxIO"
+    folder_out="$this_root/TermuxIO"
 fi
+
+mkdir -p $folder_out
+this_root="$termux_root/.termux/.."
+path_current="$this_root/current_termux.txt"
 
 # -----------------------------------------------
 ts="$(date +%Y%m%d_%H%M%S)"
+echo "$ts" > "$path_current"
+
 paths_in="$this_root/Desktop $this_root/.config $this_root/.termux"
 path_out="$folder_out/$ts.tar.gz"
-echo $ts > current_termux.txt
 
 echo "Packing to $path_out"
-tar -czf $path_out $paths_in 2>/dev/null
-
+tar -czf "$path_out" $paths_in
 
 # -----------------------------------------------
--- utilities/termux-io/unpack_termux.sh
+++ utilities/termux-io/unpack_termux.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
+this_root="$(pwd)"
+folder_io=$(echo $0 | cut -d'/' -f 1)
 termux_root="/data/data/com.termux/files/home"
-this_root="$(pwd)/.."
-
-if [[ "$termux_root/Desktop/.." == "$this_root" ]]; then # runing inside termux
-    folder_in="$termux_root/storage/shared/TermuxIO"
-else
-    folder_in="$this_root/../../TermuxIO"
-fi
-
-# Find the file with the highest numerical value i.e. most recent tar.gz
-name_in=$(ls -v "$folder_in" | tail -n 1)
-
-# Prepend the folder path to the file name
-path_in="$folder_in/$name_in"
-
-echo "Unpacking from $path_in"
 
 # -----------------------------------------------
-tempsh="../temp.sh"
-echo "#!/bin/bash" > $tempsh
+if [[ "$this_root" == "$termux_root" ]]; then # runing inside termux
+    folder_in="$this_root/storage/shared/TermuxIO"
+else
+    folder_in="$this_root/TermuxIO"
+fi
 
-echo "root=\"$this_root\"" >> $tempsh # Define the parent as root directory
-echo "tar -xzf \"$path_in\" -C \"\$root\"" >> $tempsh # Extract the contents of the tar.gz file into the root directory
-echo "echo \"$name_in\" > current_termux.txt" >> $tempsh
-echo "rm \$0" >> $tempsh # Remove the temporary script
+mkdir -p $folder_in
+this_root="$termux_root/.termux/.."
+path_current="$this_root/current_termux.txt"
 
-chmod +x $tempsh
-./$tempsh
+# -----------------------------------------------
+name_in=$(ls -v "$folder_in" | tail -n 1) # Find the file with the highest numerical value i. e. most recent tar.gz
+echo "$name_in" > "$path_current"
+
+path_in="$folder_in/$name_in" # Prepend the folder path to the file name
+path_out="$this_root"
+
+echo "Unpacking from $path_in"
+tar -xzf "$path_in" -C "$path_out" # Extract the contents of the tar.gz file into the root directory
+
+# -----------------------------------------------
